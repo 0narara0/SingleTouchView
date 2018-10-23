35b30638fd5fb892202d49a0846a4288b85108b1
EDU <0narara0@gmail.com>
2018-10-23 13:35:32 +0900

    Initial commit

---
+++ app/src/main/java/com/cmy/www/singletouchview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cmy.www.singletouchview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SingleTouchView(this,null));
+    }
+}
